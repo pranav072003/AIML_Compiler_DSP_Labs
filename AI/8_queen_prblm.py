e97ebcf1a25fb0c8e11b6106911c14f27053dfe0
pranav072003 <142641481+pranav072003@users.noreply.github.com>
2023-09-26 21:16:53 +0530

    Add files via upload

---
+++ AI/8_queen_prblm.py
@@ -0,0 +1,12 @@
+def queens(n, i, a, b, c):
+    if i < n:
+        for j in range(n):
+            if j not in a and i+j not in b and i-j not in c:
+                yield from queens(n, i+1, a+[j], b+[i+j], c+[i-j])
+    else:
+        yield a
+
+i = 0
+for solution in queens(8, 0, [], [], []):
+    print(solution)
+    i += 1
